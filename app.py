bb53323bf69485d275b57d046b4975d802e681c4
cuervo88 <cuervo.ignacio@gmail.com>
2022-08-15 20:39:11 +0200

    change in app, not stored

--- app.py
+++ app.py
@@ -21,12 +21,7 @@ from plotly.subplots import make_subplots
 from dash_extensions.enrich import Dash, ServersideOutput
 import time
 pyLogo = Image.open("eurostat_logo.png")
-#image_filename = 'img/eurostat_logo.svg' 
-#encoded_image = base64.b64encode(open(image_filename, 'rb').read()).decode()
 
-#for css in ['./assets/main.css',
-#            'https://use.fontawesome.com/releases/v5.8.1/css/all.css']:
-#            app.css.append_css({"external_url": css})
 external_stylesheets = ['https://use.fontawesome.com/releases/v5.8.1/css/all.css','https://codepen.io/chriddyp/pen/bWLwgP.css','https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css']
 app = Dash(__name__, external_stylesheets = external_stylesheets )
 
@@ -71,58 +66,22 @@ head_site = html.Div([
 #About
 
 
-#First plot
 
-#plot1 = html.Div([
-#    html.Div([
-#        html.H2("Plot1")
-#        ]),
-#    html.Div([
-#        html.Div("chart1",className="plot1-col"),
-#        html.Div("chart2",className="plot1-col")
-#    ], className="row")
-#],className="plot1",id="plot1")
 end = html.Div([html.H2("Authors", style={'text-align':'center'}),
     dcc.Markdown('''#### Ignacio Cuervo (@cuervo88) and Daniel Rodriguez Gutierrez (@Dannyovi).''',style={'text-align':'center'})],className="container-fluid footer bg-dark")
 app.layout = html.Div([head_site,plots,end])
 
 #Callback-----------------------------------------------------------------------------------------------------------------
-#@app.callback(Output(component_id='tabs_child', component_property='children'), 
-#[Input(component_id='button1',component_property='n_clicks'),
-#Input(component_id='button2',component_property='n_clicks'),
-#Input(component_id='button3',component_property='n_clicks')])
-#def display(but1,but2,but3):
-#    if but1:
-#        return plot1
-#    if but2:
-#        pass
-#    if but3:
-#        pass
 
-#@app.callback(Output(component_id='year_rev_selected', component_property='data'), 
-#Input(component_id='Year',component_property='value'))
-#def update_graph(year_slctd):
-#    df = map_year_imp_exp(year_slctd)
-#    df['ISO3'] = df['iso2'].map(iso_to_iso)
-#    df['name'] = df['iso2'].map(iso_to_name)
-#    df2_pivot = df.pivot(index=['ISO3',"name"], columns="flow", values="sum_")
-#    df2_pivot["REV"] = df2_pivot["2"]-df2_pivot["1"]
-#    df2_pivot.reset_index(inplace=True)
-#    return df2_pivot.to_json(date_format='iso', orient='split')
-@app.callback(ServersideOutput("store", "data"), [Input(component_id='ImpExp',component_property='value'),
-    Input(component_id='Year',component_property='value')])
-def query_data(impexp, year):
+
+
+@app.callback(Output(component_id="trading_map", component_property="figure"), [Input(component_id='Year',component_property='value'), Input(component_id="ImpExp",component_property= "value"),
+    Input(component_id='ranking_chart',component_property='selectedData')])
+def graph1(year,impexp,select):
     df2_pivot = df2.pivot(index=['ISO3',"name"], columns="flow", values="sum_"+year)
     df2_pivot["REV"] = df2_pivot.iloc[:,1]-df2_pivot.iloc[:,0]
     df2_pivot.reset_index(inplace=True)
     df2_pivot.sort_values(by=[impexp], axis=0, ascending=False, inplace=True)
-    time.sleep(1)
-    return df2_pivot
-
-@app.callback(Output(component_id="trading_map", component_property="figure"), [Input(component_id="store", component_property="data"), Input(component_id="ImpExp",component_property= "value"),
-    Input(component_id='ranking_chart',component_property='selectedData')])
-def graph1(df,impexp,select):
-    df2_pivot = df
     a = generate_rgb(impexp,df2_pivot[impexp])
     
 
@@ -140,8 +99,6 @@ def graph1(df,impexp,select):
         mapbox_style="carto-positron",
         hover_data=['name',impexp],
         color_continuous_scale=a,
-        #labels={'Name':'Thousands Euro'},
-        #template='seaborn'
     )
 
     if select is not None:
@@ -149,73 +106,11 @@ def graph1(df,impexp,select):
         fig_map.data[0].update(selectedpoints=[click_data])
     else:
         fig_map.data[0].update(selectedpoints=False)
-        #res = re.sub('""','',click_data)
-        # for c in range(len(fig_map["data"][0]["customdata"])):
-        #     if fig_map["data"][0]["customdata"][c][0] != click_data:
-        #         fig_map.data[0].update(selectedpoints=[c])
-    # color_list = list(fig_map["layout"]['coloraxis']['colorscale'])
-    # color_list[c] = str(fig_map["layout"]['coloraxis']['colorscale'][c][0])'rgb(255,255,255)'
-    # a[c] = 'rgb(255,255,255)'
-    # if impexp == "REV":
-    #     fig_map= px.choropleth_mapbox(df2_pivot,
-    #     geojson=euro_map,
-    #     width=Width,
-    #     height=Height,
-    #     locations='ISO3',
-    #     title= 'Trading amount (thousand Euro)',
-    #     center={"lat": 56, "lon": 10},
-    #     zoom=2.8,
-    #     opacity=0.9,
-    #     color=impexp,
-    #     mapbox_style="carto-positron",
-    #     hover_data=['name',impexp],
-    #     color_continuous_scale=px.colors.diverging.PRGn,
-    #     color_continuous_midpoint=0,
-    #     #labels={'Name':'Thousands Euro'},
-    #     #template='seaborn'
-    #     )
-    # elif impexp == 2:
-    #     df2_pivot.sort_values(by=[impexp], axis=0, ascending=False, inplace=True)
-    #     fig_map= px.choropleth_mapbox(
-    #         data_frame=df2_pivot,
-    #         geojson=euro_map,
-    #         width=Width,
-    #         height=Height,
-    #         locations='ISO3',
-    #         title= 'Trading amount (thousand Euro)',
-    #         center={"lat": 56, "lon": 10},
-    #         zoom=2.8,
-    #         opacity=0.9,
-    #         color=impexp,
-    #         mapbox_style="carto-positron",
-    #         hover_data=['name',impexp],
-    #         color_continuous_scale=px.colors.sequential.BuGn,
-    #         #labels={'Name':'Thousands Euro'},
-    #         #template='seaborn'
-    #     )
-    # else:
-    #     df2_pivot.sort_values(by=[impexp], axis=0, ascending=False, inplace=True)
-    #     fig_map= px.choropleth_mapbox(
-    #         data_frame=df2_pivot,
-    #         geojson=euro_map,
-    #         width=Width,
-    #         height=Height,
-    #         locations='ISO3',
-    #         title= 'Trading amount (thousand Euro)',
-    #         center={"lat": 56, "lon": 10},
-    #         zoom=2.8,
-    #         opacity=0.9,
-    #         color=impexp,
-    #         mapbox_style="carto-positron",
-    #         hover_data=['name',impexp],
-    #         color_continuous_scale=px.colors.sequential.BuPu,
-    #         #labels={'Name':'Thousands Euro'},
-    #         #template='seaborn'
-    #     )
+        
     impexpval = np.around(df2_pivot[impexp].values/1000000)
     customdata  = np.stack((df2_pivot['name'],impexpval), axis=-1)
 
-    #fig_map.update_traces(hovertemplate=None)
+ 
     fig_map.update_traces(hovertemplate='Country: %{customdata[0]} <br>Amount: %{customdata[1]} Million €')
     fig_map.update_layout(hoverlabel=dict(
         bgcolor="black",
@@ -241,54 +136,7 @@ def graph2(impexp, year,click):
                     font_family="Arial")
     ))
 
-    # if impexp == "REV":
-    #     fig2 = px.bar(
-    #         df2_pivot,
-    #         width=Width,
-    #         height=Height,
-    #         x='name', 
-    #         y=impexp,
-    #         color=impexp,
-    #         labels={"sum_":'Thousands Euro'},
-    #         hover_data=['name', impexp],
-    #         color_continuous_scale=px.colors.diverging.PRGn,
-    #         color_continuous_midpoint=0,
-    #         color_discrete_map={"0":"gray","1":"blue"},
-    #         title= 'Ranking (thousand Euro)',
-    #         template='plotly_white'
-    #     )
-    # elif impexp == 2:
-    #     df2_pivot.sort_values(by=[impexp], axis=0, ascending=False, inplace=True)
-    #     fig2 = px.bar(
-    #     df2_pivot,
-    #     width=Width,
-    #     height=Height,
-    #     x='name', 
-    #     y=impexp,
-    #     color=impexp,
-    #     labels={"sum_":'Thousands Euro'},
-    #     hover_data=['name', impexp],
-    #     color_continuous_scale=px.colors.sequential.BuGn,
-    #     title= 'Exporting countries',
-    #     template='plotly_white')
-    # else:
-    #     df2_pivot.sort_values(by=[impexp], axis=0, ascending=False, inplace=True)
-    #     fig2 = px.bar(
-    #         df2_pivot, 
-    #         width=Width,
-    #         height=Height,
-    #         x='name', 
-    #         y=impexp,
-    #         color=impexp,
-    #         labels={"sum_":'Thousands Euro'},
-    #         hover_name='name',
-    #         hover_data=[impexp],
-    #         color_continuous_scale=px.colors.sequential.BuPu, 
-    #         title= 'Importing countries',
-    #         template='plotly_white')
-
-    # customdata  = np.transpose([df2_pivot['name'],df2_pivot[impexp]])
-    # fig2.update_traces(hovertemplate='Country: %{customdata} <br>Amount: %{customdata[1]} Million €')
+   
 
     fig2.update_layout(autosize=True,paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',title_font_color="black", font_color="black",clickmode='event+select',showlegend=False,
     xaxis ={'showgrid': False},yaxis = {'showgrid': False})
@@ -416,36 +264,6 @@ def table_and_fig(prod1,year):
         plot_bgcolor='rgba(0,0,0,0)',annotations=annotations
     )
 
-#    fig2 = px.bar(df4[df4["flow"]==2], y="name", x="sum_"+year,orientation='h')
-#    fig1.update_layout(yaxis=dict(autorange="reversed"))
-#    fig2.update_layout(yaxis=dict(autorange="reversed"))
-#    names = list(df_pivot["name"])
-#    Exp = list(np.around(np.array(df_pivot[2])/1000000, decimals=1))
-#   table = go.Figure(data=[go.Table(
-#        columnwidth = [150,250,250],
-#        header=dict(values=['Country', 'Import (million €)','Export (million €)'],
-#                    font=dict(color='black', size=16),
-#                    fill_color='white',
-#                    align=['left','center'],
-#                    height=40),
-#        cells=dict(values=[names,Imp,Exp],
-#                font=dict(color='black', size=12),
-#                fill_color='white',
-#                align='left',
-#                height=25))
-#    ])
-#    fig1.update_xaxes(showticklabels=True)
-#    fig1.update_layout(annotations=annotations,height=800,title="Countries importing {}".format(prod[prod1].capitalize()),
-#    xaxis_title="Amount in €",
-#    yaxis_title="",
-#    paper_bgcolor='rgba(0,0,0,0)',
-#    plot_bgcolor='rgba(0,0,0,0)',
-#    title_font_color="black",font_color="black",clickmode='event+select')
-#    fig2.update_layout(height=800,title="Countries exporting {}".format(prod[prod1].capitalize()),
-#    xaxis_title="Amount in €",
-#    yaxis_title="",
-#    paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',title_font_color="black",font_color="black",clickmode='event+select')
-#    table.update_layout(margin=dict(l=10, r=0, t=0, b=0),height=990,paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',title_font_color="black",font_color="black",clickmode='event+select')
     return fig 
 @app.callback(Output(component_id='imp-exp-perc', component_property='figure'),
     Input(component_id='Year',component_property='value'))
